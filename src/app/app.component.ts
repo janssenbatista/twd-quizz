f913a92e043497f7d5a68e8c19a5567646f027d6
Janssen Batista <batistajanssen.dev@gmail.com>
2024-02-02 20:24:33 -0300

    feat: update app component
    add quizz component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { QuizzComponent } from './quizz/quizz.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, QuizzComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'twd-quizz';
   isPlaying: boolean = true;
+
+  constructor() {}
 
   toggleSound() {
     this.isPlaying = !this.isPlaying;
