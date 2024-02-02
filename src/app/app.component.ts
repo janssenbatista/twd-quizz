bd2e8dbe7d8a2e8474e758653c123103238c8e84
Janssen Batista <batistajanssen.dev@gmail.com>
2024-02-01 21:22:49 -0300

    feat: add sound to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,13 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'twd-quizz';
+  isPlaying: boolean = true;
+
+  toggleSound() {
+    this.isPlaying = !this.isPlaying;
+  }
 }
